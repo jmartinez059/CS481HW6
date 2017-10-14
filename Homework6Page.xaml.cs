e3dc958b44b5dc60b6d8a344ca9115b890baab6f
jmartinez059 <32280998+jmartinez059@users.noreply.github.com>
2017-10-13 19:37:08 -0700

    Add files via upload

--- Homework6Page.xaml.cs
+++ Homework6Page.xaml.cs
@@ -6,42 +6,68 @@ using Xamarin.Forms.Maps;
 
 namespace Homework6
 {
-    public partial class Homework6Page : ContentPage
-    {
-        public Homework6Page()
-        {
-            InitializeComponent();
-            PopulatePicker();
-            Set_Map_Location(33.13005900000001, -117.16420099999999);
+	public partial class Homework6Page : ContentPage
+	{
+		public Homework6Page()
+		{
+			InitializeComponent();
+			PopulatePicker();
+			Set_Map_Location(33.13005900000001, -117.16420099999999);
+		}
+
+		private void PopulatePicker()
+		{
+			var personList = new ObservableCollection<Person>()
+			{
+				new Person() {firstName = "Jason", lastName = "Martinez", address = "305 S.Nevada St, Oceanside, CA 92054",
+					lati = 33.1938885, longi = -117.376778},
+				new Person() {firstName = "Roger", lastName = "Elmwood", address = "4415 Jill St, Oceanside, CA 92057",
+					lati = 33.2459702, longi = -117.3200064},
+				new Person() {firstName = "Virginia", lastName = "Longformore", address = "2601 Jefferson St, Carlsbad, CA 92008",
+					lati = 33.166077, longi = -117.34831},
+
+			};
+			PickerPerson.ItemsSource = personList;
+		}
+
+		public void Handle_SelectedIndexChanged(object sender, System.EventArgs e)
+		{
+            var person = (Person)PickerPerson.SelectedItem;
+			var longitude = person.longi;
+			var latitude = person.lati;
+			Set_Map_Location(latitude, longitude);
+
+            var pin = new Pin()
+			{
+				Position = new Position(latitude, longitude),
+				Label = person.firstName,
+
+			};
+			MyMap.Pins.Add(pin);
+		}
+
+		private void Set_Map_Location(double latitude, double longitude)
+		{
+			var newLocation = MapSpan.FromCenterAndRadius(new Position(latitude, longitude), Distance.FromMiles(1));
+			MyMap.MoveToRegion(newLocation);
+
         }
 
-        private void PopulatePicker()
+        void Handle_PropertyChanged(object sender, System.EventArgs e)
         {
-            var personList = new ObservableCollection<Person>()
+            var mapType = (String)PickerMapType.SelectedItem;
+            if(mapType == "Satellite")
             {
-                new Person() {firstName = "Jason", lastName = "Martinez", address = "305 S.Nevada St, Oceanside, CA 92054",
-                    lati = 33.1938885, longi = -117.376778},
-                new Person() {firstName = "Roger", lastName = "Elmwood", address = "4415 Jill St, Oceanside, CA 92057",
-                    lati = 33.2459702, longi = -117.3200064},
-                new Person() {firstName = "Virginia", lastName = "Longformore", address = "2601 Jefferson St, Carlsbad, CA 92008",
-                    lati = 33.166077, longi = -117.34831},
-
-            };
-            PickerPerson.ItemsSource = personList;
-        }
-
-         public void Handle_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
-            var person = (Person)sender;
-            var longitude = person.longi;
-            var latitude = person.lati;
-            Set_Map_Location(latitude, longitude);
-        }
-
-        private void Set_Map_Location(double latitude, double longitude)
-        {
-            var newLocation = MapSpan.FromCenterAndRadius(new Position(latitude, longitude), Distance.FromMiles(1));
-            MyMap.MoveToRegion(newLocation);
+                MyMap.MapType = MapType.Satellite;
+            }
+            if (mapType == "Street")
+            {
+                MyMap.MapType = MapType.Street;
+            }
+            else
+            {
+                MyMap.MapType = MapType.Hybrid;
+            }
         }
     }
 }
